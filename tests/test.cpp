861b830567465174e74e771334da9a770a892df3
limenitis <ini.ivi@yandex.ru>
2021-02-20 18:49:51 +0300

    add tests for str-tools and Schedule

--- tests/test.cpp
+++ tests/test.cpp
@@ -130,14 +130,12 @@ namespace test_str_tools
   }
 
   TEST(Checks, is_char) {
-    for (char i = 'а'; i > 'я'; i++)
-    {
-      EXPECT_TRUE(is_char(i));
-    } 
 
-    for (int i = 'a'; i < 'z'; i++)
+    std::string str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+    for(auto i : str) 
     {
-      EXPECT_TRUE(is_char(i));
+        EXPECT_TRUE(is_char(i));
     }
 
     EXPECT_FALSE(is_char('0'));
@@ -165,17 +163,97 @@ namespace test_str_tools
   	EXPECT_TRUE(is_int_char('9'));
   }
 
-  //bool is_int_char(char);
-  //bool is_char(char);
-  //int  len_num(int);
-  //int  len_str(char*);
+  TEST(Checks, len_str) {
+  	EXPECT_EQ(6, len_str("000000"));
+  	EXPECT_EQ(6, len_str("asfrfa"));
+  	EXPECT_EQ(6, len_str("3$%7^&"));
+  	EXPECT_EQ(6, len_str("3$2%Fd"));
+  	EXPECT_EQ(6, len_str("%2aQ3%"));
+  	EXPECT_EQ(6, len_str("%S3$1f"));
+  	EXPECT_EQ(6, len_str("ref34a"));
+  	EXPECT_NE(6, len_str("вфаУ42"));
+    EXPECT_NE(6, len_str("%вЦ;1:"));
+    EXPECT_NE(6, len_str("Ац4В,."));
+  }
 
+  TEST(Checks, len_num) {
+  	EXPECT_EQ(9, len_num(111111111));
+  	EXPECT_EQ(9, len_num(123456789));
+  	EXPECT_EQ(9, len_num(987654321));
+  	EXPECT_EQ(9, len_num(345736831));
+  	EXPECT_EQ(9, len_num(947275826));
+  	EXPECT_EQ(6, len_num(   111111));
+    EXPECT_EQ(6, len_num(   348374));
+    EXPECT_EQ(6, len_num(   237855));
+  	EXPECT_EQ(1, len_num(        1));
+  	EXPECT_EQ(1, len_num(        0));
+  }
 
-  //bool  is_numstr(char*);
-  //bool  is_charstr(char*);
-  //int   str2num(char*);
-  //char* num2str(int);
+  TEST(Checks, is_numstr) {
+  	EXPECT_TRUE  (is_numstr("0484937985"));
+  	EXPECT_TRUE  (is_numstr("0123456789"));
+  	EXPECT_TRUE  (is_numstr("1234567890"));
+  	EXPECT_TRUE  (is_numstr("4020572057"));
+  	EXPECT_TRUE  (is_numstr("8402750740"));
+  	EXPECT_FALSE (is_numstr("3957d94759"));
+    EXPECT_FALSE (is_numstr("d948028570"));
+    EXPECT_FALSE (is_numstr("klsdli4038"));
+  	EXPECT_FALSE (is_numstr("387502705f"));
+  	EXPECT_FALSE (is_numstr("nducnriwxo"));
+  }
 
+  TEST(Checks, is_charstr) {
+  	EXPECT_FALSE (is_charstr("0484937985")); 
+  	EXPECT_FALSE (is_charstr("0123456789")); 
+  	EXPECT_FALSE (is_charstr("1234567890")); 
+  	EXPECT_FALSE (is_charstr("4020572057")); 
+  	EXPECT_FALSE (is_charstr("8402750740")); 
+  	EXPECT_FALSE (is_charstr("3957d94759")); 
+    EXPECT_FALSE (is_charstr("d948028570")); 
+    EXPECT_FALSE (is_charstr("klsdli4038")); 
+  	EXPECT_FALSE (is_charstr("387502705f")); 
+    EXPECT_FALSE (is_charstr("ALR.dimros"));
+    EXPECT_TRUE  (is_charstr("nducnriwxo"));
+    EXPECT_TRUE  (is_charstr("dkrmriMEIC")); 
+    EXPECT_TRUE  (is_charstr("DJOSndjrno")); 
+  }
+
+  TEST(Checks, str2num) {
+  	EXPECT_EQ( 484937985, str2num("0484937985")); 
+  	EXPECT_EQ( 123456789, str2num("0123456789")); 
+  	EXPECT_EQ(1234567890, str2num("1234567890")); 
+  	EXPECT_EQ(4020572057, str2num("4020572057")); 
+  	EXPECT_EQ(8402750740, str2num("8402750740")); 
+  	EXPECT_EQ(1111111111, str2num("1111111111")); 
+  	EXPECT_EQ(9857654321, str2num("9857654321")); 
+  	EXPECT_EQ(3457836831, str2num("3457836831")); 
+  	EXPECT_EQ(9472725826, str2num("9472725826")); 
+  	EXPECT_EQ(     25826, str2num("0000025826")); 
+
+    EXPECT_NE(   1111111, str2num("   1111111")); // return -579019705 ( because spaces )
+    EXPECT_NE(   3486374, str2num("   3486374")); // return -576644442 ( because spaces )
+    EXPECT_NE(   2378355, str2num("   2378355")); // return -577752461 ( because spaces )
+  	EXPECT_NE(         1, str2num("         1")); // return -597908575 ( because spaces )
+  	EXPECT_NE(         0, str2num("         0")); // return -597908576 ( because spaces )
+  }
+
+  TEST(Checks, num2str) {
+  	EXPECT_STREQ( "484937985", num2str( 484937985)); 
+  	EXPECT_STREQ( "123456789", num2str( 123456789)); 
+  	EXPECT_STREQ("1234567890", num2str(1234567890)); 
+  	EXPECT_STREQ("4020572057", num2str(4020572057)); 
+  	EXPECT_STREQ("8402750740", num2str(8402750740)); 
+  	EXPECT_STREQ("1111111111", num2str(1111111111)); 
+  	EXPECT_STREQ("9857654321", num2str(9857654321)); 
+  	EXPECT_STREQ("3457836831", num2str(3457836831)); 
+  	EXPECT_STREQ("9472725826", num2str(9472725826)); 
+  	EXPECT_STREQ(     "25826", num2str(     25826)); 
+  	EXPECT_STREQ(   "1111111", num2str(   1111111)); 
+    EXPECT_STREQ(   "3486374", num2str(   3486374)); 
+    EXPECT_STREQ(   "2378355", num2str(   2378355)); 
+  	EXPECT_STREQ(         "1", num2str(         1)); 
+  	EXPECT_STREQ(         "0", num2str(         0)); 
+  }
 }
 
 namespace test_Structures
@@ -196,14 +274,12 @@ namespace test_Structures
         EXPECT_STREQ("15.01.2020", obj.get_date());
     }
 
-    TEST(TestDate, SetDate_str_WithPositive) {
+    TEST(TestDate, SetDate_str) {
         Date obj;
+
         obj.set_date(80, 80, -100);
         EXPECT_STREQ("00.00.0000", obj.get_date());
-    }
 
-    TEST(TestDate, SetDate_str_WithNegative) {
-        Date obj;
         obj.set_date(-1, -1, -10);
         EXPECT_STREQ("00.00.0000", obj.get_date());
     }
@@ -248,29 +324,29 @@ namespace test_Structures
 
     // ==============================================
 
-    TEST(RegistrationNumber, Constructor_Default) {
+    TEST(TestRegistrationNumber, Constructor_Default) {
         RegistrationNumber obj;
         EXPECT_STREQ("00-0000000", obj.get_reg());
     }
 
-    TEST(RegistrationNumber, Constructor_int) {
+    TEST(TestRegistrationNumber, Constructor_int) {
         RegistrationNumber obj(12, 353);
         EXPECT_STREQ("12-0000353", obj.get_reg());
     }
 
-    TEST(RegistrationNumber, Constructor_str) {
+    TEST(TestRegistrationNumber, Constructor_str) {
         char str[11]{ '1', '2', '-', '3', '4', '5', '6', '7', '8', '9', '\0' };
         RegistrationNumber obj(str);
-        EXPECT_STREQ("12-3456789", obj.get_reg()); // "00-0000000"
+        EXPECT_STREQ("12-3456789", obj.get_reg());
     }
 
-    TEST(RegistrationNumber, SetDate_str) {
+    TEST(TestRegistrationNumber, SetDate_str) {
         RegistrationNumber obj;
         obj.set_reg(20, 50);
         EXPECT_STREQ("20-0000050", obj.get_reg());
     }
 
-    TEST(RegistrationNumber, SetDate_int) {
+    TEST(TestRegistrationNumber, SetDate_int) {
         RegistrationNumber obj;
         obj.set_reg(00, 2552302);
         EXPECT_STREQ("00-2552302", obj.get_reg());
@@ -278,32 +354,35 @@ namespace test_Structures
 
     // ==============================================
 
-    TEST(Schedule, Constructor_Default) {
-        //Schedule obj;
-        //EXPECT_STREQ("12-3456789", obj.get_schedule());
+    TEST(TestSchedule, Constructor_Default) {
+        Schedule obj;
+        EXPECT_STREQ("00:00-00:00", obj.get_schedule());
     }
 
-    TEST(Schedule, Constructor_int) {
-        //Schedule obj();
-        //EXPECT_STREQ("12-0000353", obj.get_schedule());
+    TEST(TestSchedule, Constructor_Time) {
+        Time t1(10 ,00);
+        Time t2(19, 00);
+        Schedule obj(t1, t2);
+        EXPECT_STREQ("10:00-19:00", obj.get_schedule());
     }
 
-    TEST(Schedule, Constructor_str) {
-        //char str[11]{ '1', '2', '-', '3', '4', '5', '6', '7', '8', '9', '\0' };
-        //Schedule obj(str);
-        //EXPECT_STREQ("12-3456789", obj.get_schedule());
+    TEST(TestSchedule, Constructor_str) {
+        char str[12] = {'1', '0', ':', '0', '0', '-', '1', '8', ':', '0', '0', '\0'};        
+        Schedule obj(str);
+        EXPECT_STREQ("10:00-18:00", obj.get_schedule());
     }
 
-    TEST(RegistratScheduleionNumber, SetDate_str) {
-        //Schedule obj;
-        //obj.set_schedule("12:00-15:00");
-        //EXPECT_STREQ("12:00-15:00", obj.get_schedule());
+    TEST(TestSchedule, SetDate_str) {
+        Schedule obj;
+        obj.set_schedule("12:00-15:00");
+        EXPECT_STREQ("12:00-15:00", obj.get_schedule());
     }
 
-    TEST(Schedule, SetDate_int) {
-        //Schedule obj;
-        //obj.set_schedule(00, 2552302);
-        //EXPECT_STREQ("00-2552302", obj.get_schedule());
+    TEST(TestSchedule, SetDate_Time) {
+        Time t1(10, 00);
+        Time t2(16, 00);
+        Schedule obj(t1, t2);
+        EXPECT_STREQ("10:00-16:00", obj.get_schedule());
     }
 
 }
