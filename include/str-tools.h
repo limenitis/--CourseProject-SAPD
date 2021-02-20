1d4f198be80fc78e6771f69e31764dff838a6ec1
limenitis <ini.ivi@yandex.ru>
2021-02-20 18:48:34 +0300

    fix data types

--- include/str-tools.h
+++ include/str-tools.h
@@ -2,22 +2,24 @@
 
 // for symbols - >
 
-bool is_int      (int );
-bool is_char     (char);
-bool is_int_char (char);
-char int2char (int );
-int  char2int (char);
+bool is_int      (int32_t);
+bool is_char     (char   );
+bool is_int_char (char   );
+
+char     int2char (int16_t);
+int16_t  char2int (char   );
 
 // for lines - >
 
 bool is_numstr  (char*);
 bool is_charstr (char*);
-char* num2str (int  );
-int   str2num (char*);
 
-int  len_num(int);
-int  len_str(char*);
-void outstr(char*);
+char*   num2str (int64_t);
+int64_t str2num (char*  );
+
+int64_t len_num(int64_t);
+int64_t len_str(char*);
+void    outstr(char*);
 
 bool compare_str_left_over(char*, char*);
 bool compare_str_right_over(char*, char*);
--- src/str-tools.cpp
+++ src/str-tools.cpp
@@ -13,9 +13,9 @@ bool is_int_char(char num)
     return false;
 }
 
-bool is_int(int num)
+bool is_int(int32_t num)
 {
-    int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int32_t nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     for (auto i : nums)
     {
         if (num == i)
@@ -28,55 +28,55 @@ bool is_int(int num)
 
 bool is_char(char c)
 {
-    return (('a' <= c) && (c <= 'z') || ('A' <= c) && (c <= 'Z') || ('а' <= c) && (c <= 'я') || ('А' <= c) && (c <= 'Я'));
+    return ('a' <= c) && (c <= 'z') || ('A' <= c) && (c <= 'Z');
 }
 
-char int2char(int i) // 48 char == 0 int (ASCII)
+char int2char(int16_t i) // 48 char == 0 int (ASCII)
 {
     return i + 48;
 }
 
-int char2int(char c) // 48 char == 0 int (ASCII)
+int16_t char2int(char c) // 48 char == 0 int (ASCII)
 {
     return c - 48;
 }
 
-int len_num(int num)
+int64_t len_num(int64_t num)
 {
-    int i = 1;
-    while (num / int(pow(10, i)) != 0) { i++; }
+    int64_t i = 1;
+    while (num / int64_t(pow(10, i)) != 0) { i++; }
     return i;
 }
 
-int len_str(char* str)
+int64_t len_str(char* str)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str[i] != '\0') { i++; }
     return i;
 }
 
-char* num2str(int num)
+char* num2str(int64_t num)
 {
-    int len = len_num(num);
+    int64_t len = len_num(num);
     char* str = new char[len+1]{'\0'};
 
-    for (int i = len-1; i >= 0; i--)
+    for (int64_t i = len-1; i >= 0; i--)
     {
-        str[len-i-1] = int2char(num / int(pow(10, i)) % 10);
+        str[len-i-1] = int2char(num / int64_t(pow(10, i)) % 10);
     }
 
     return str;
 }
 
-int str2num(char* str)
+int64_t str2num(char* str)
 {
-    int num = 0;
-    int len = len_str(str);
+    int64_t num = 0;
+    int64_t len = len_str(str);
 
-    int i = 0;
+    int64_t i = 0;
     while (str[i] != '\0')
     {
-        num += char2int(str[i]) * pow(10, len-i-1);
+        num += char2int(str[i]) * (int64_t)pow(10, len-i-1);
         i++;
     }
 
@@ -85,7 +85,7 @@ int str2num(char* str)
 
 bool is_numstr(char* str)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str[i] != '\0')
     {
         if (is_int_char(str[i]) == false)
@@ -99,7 +99,7 @@ bool is_numstr(char* str)
 
 bool is_charstr(char* str)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str[i] != '\0')
     {
         if (is_char(str[i]) == false)
@@ -113,7 +113,7 @@ bool is_charstr(char* str)
 
 void outstr(char* str)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str[i] != '\0')
     {
         std::cout << str[i];
@@ -124,7 +124,7 @@ void outstr(char* str)
 
 bool compare_str_left_over(char* str1, char* str2)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str1[i] != '\0' && str2[i] != '\0')
     {
         if (str1[i] > str2[i])
@@ -139,7 +139,7 @@ bool compare_str_left_over(char* str1, char* str2)
 
 bool compare_str_right_over(char* str1, char* str2)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str1[i] != '\0' && str2[i] != '\0')
     {
         if (str1[i] < str2[i])
@@ -154,7 +154,7 @@ bool compare_str_right_over(char* str1, char* str2)
 
 bool compare_str_equal(char* str1, char* str2)
 {
-    int i = 0;
+    int64_t i = 0;
     while (str1[i] != '\0' && str2[i] != '\0')
     {
         if (str1[i] != str2[i])
