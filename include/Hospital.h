a80e4190720b90892d34099a345f574607fa5201
limenitis <ini.ivi@yandex.ru>
2021-02-21 18:35:44 +0300

    create God Class (or Interface) Hospital

--- include/Hospital.h
+++ include/Hospital.h
@@ -1,10 +1,49 @@
 #pragma once
-// #include <fstream>
-// #include "str-tools.h"
+// #include ".\..\include\Time.h"
+// #include ".\..\include\Date.h"
+// #include ".\..\include\Schedule.h"
+// #include ".\..\include\RegistrationNumber.h"
+#include ".\..\include\HashTable.h"
+#include ".\..\include\LinkedList.h"
+#include ".\..\include\AVLTree.h"
+// #include ".\..\include\str-tools.h"
+
+#include ".\..\include\Doctor.h"
+#include ".\..\include\Patient.h"
+#include ".\..\include\Direction.h"
+
 #include <iostream>
+// #include <fstream>
 using namespace std;
 
 
 class Hospital{
+private:
+    HashTable *table = new HashTable;
+
+public:
+    Hospital();
+    ~Hospital();
+
+    bool add_patient    ( void );
+    bool remove_patient ( void );
+    bool edit_patient   ( void );
+    void print_patients ( void );
+    bool clear_patients ( void );
+    bool find_patient_by_name    ( void );
+    bool find_patient_by_reg_num ( void );
+
+
+    bool add_doctor    ( void );
+    bool remove_doctor ( void );
+    bool edit_doctor   ( void );
+    void print_doctors ( void );
+    bool clear_doctors ( void );
+    bool find_doctor_by_name ( void );
+    bool find_doctor_by_post ( void );
+
+    bool add_direction    ( void ); 
+    bool return_direction ( void );
+
 };
 
--- src/Hospital.cpp
+++ src/Hospital.cpp
@@ -1,2 +1,100 @@
 #include ".\..\include\Hospital.h"
 
+Hospital::Hospital()
+{}
+
+Hospital::~Hospital()
+{
+    delete table;
+}
+
+bool Hospital::add_patient ()
+{
+    Patient new_patient;
+    cin >> new_patient;
+
+    HashTableNode element;
+    element.intsert(new_patient);
+
+    return table->insert(element);
+}
+
+bool Hospital::remove_patient ()
+{
+    Patient new_patient;
+    cin >> new_patient;
+
+    HashTableNode element;
+    element.intsert(new_patient);
+
+    return table->remove(element);
+}
+
+bool Hospital::edit_patient ()
+{
+    return true;
+}
+
+void Hospital::print_patients  ( void )
+{
+    table->print();
+}
+
+bool Hospital::clear_patients  ( void )
+{
+    return true;
+}
+
+bool Hospital::find_patient_by_name ()
+{
+    return true;
+}
+
+bool Hospital::find_patient_by_reg_num ()
+{
+    return true;
+}
+
+bool Hospital::add_doctor ()
+{
+    return true;
+}
+
+bool Hospital::remove_doctor ()
+{
+    return true;
+}
+
+bool Hospital::edit_doctor ()
+{
+    return true;
+}
+
+void Hospital::print_doctors  ( void )
+{}
+
+bool Hospital::clear_doctors  ( void )
+{
+    return true;
+}
+
+bool Hospital::find_doctor_by_name ()
+{
+    return true;
+}
+
+bool Hospital::find_doctor_by_post ()
+{
+    return true;
+}
+
+bool Hospital::add_direction ()
+{
+    return true;
+}
+
+bool Hospital::return_direction ()
+{
+    return true;
+}
+
