7e4f1753f4f8b6def7e2ece38f5051f9ffdcade7
limenitis <ini.ivi@yandex.ru>
2021-02-21 18:36:58 +0300

    HashTable first version

--- include/HashTable.h
+++ include/HashTable.h
@@ -8,19 +8,21 @@ using namespace std;
 class HashTableNode
 {
 private:
-	RegistrationNumber key;
-	Patient            *data;
+	//Patient *data = new Patient;
+	Patient data;
+	bool empty_data;
 	bool delete_data;
 
 public:
 	HashTableNode();
-	bool intsert (Patient* data);
+	bool intsert (Patient data);
 	bool remove  ();
 	bool empty   ( void );
 	bool deleted ( void );
-	auto get_key ( void );
 
     friend class HashTable;
+	friend bool operator==(HashTableNode p1, HashTableNode p2);
+
 };
 
 
@@ -28,16 +30,19 @@ class HashTable
 {
 private:
 	int N = 2500;
-	HashTableNode* segment[2500];
+	HashTableNode* segment = new HashTableNode[2500];
+	//HashTableNode segment[2500];
 
-	bool correct_key       (HashTableNode *data);
-	int  get_hash          (HashTableNode *data);
-	int  get_hash_conflict (HashTableNode *data);
+	bool correct_key       (HashTableNode data);
+	int  get_hash          (HashTableNode data);
+	int  get_hash_conflict (HashTableNode data);
 
 public:
-	bool insert	  (HashTableNode *data);
-	bool remove   (HashTableNode *data);
-	int  find_key (HashTableNode *data);
+	bool insert	  (HashTableNode data);
+	bool remove   (HashTableNode data);
+	int  find_key (HashTableNode data);
 	void print    (int from = 0, int to = 2500);
+
+	friend class HashTableNode;
 };
 
--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -1,35 +1,43 @@
-﻿#pragma once
-#include ".\..\include\HashTable.h"
+﻿#include ".\..\include\HashTable.h"
+#include ".\..\include\SimpleLogs.h"
 #include ".\..\include\str-tools.h"
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
 
 HashTableNode::HashTableNode()
 {
 	delete_data = false;
-	data = nullptr;
-	key  = nullptr;
+	empty_data = true; //data = nullptr;
 }
 
-bool HashTableNode::intsert (Patient* d)
+bool HashTableNode::intsert (Patient d)
 {
 	delete_data = false;
+	empty_data = false;
 	data = d;
-	key  = d->get_reg_num();
+
+	log_info("HashTable", "insert", "true");
+	return true;
 }
 
 bool HashTableNode::remove ()
 {
 	delete_data = true;
-	delete data;
-	data = nullptr;
-	key  = nullptr;
+	empty_data = true;
+	//delete data;
+	//data = nullptr;
+
+	log_info("HashTable", "remove", "true");
+	return true;
 }
 
 bool HashTableNode::empty ( void )
 {
-	return data == nullptr;
+	if (data.get_year_born() == 0)  { return true;  }
+	else                            { return false; }
 }
 
 bool HashTableNode::deleted ( void )
@@ -37,107 +45,102 @@ bool HashTableNode::deleted ( void )
 	return delete_data;
 }
 
-auto HashTableNode::get_key ( void )
-{
-	return key;
-}
-
-
-bool HashTable::insert(HashTableNode* node) 
+bool HashTable::insert(HashTableNode node) 
 {
 	if (correct_key(node)) 
 	{	
-		int c = 0;
 		int id = get_hash(node);
-		if ( (segment[id]->empty()) || (segment[id]->deleted()) ) // is empty or delete
+		if ( (segment[id].empty()) || (segment[id].deleted()) ) // is empty or delete
 		{
 			segment[id] = node;
+			log_info("HashTable", "insert", "true")
             return true;
         }
 		else 
 		{
 			id = (id + get_hash_conflict(node)) % N;
 			
-			while ( ((!segment[id]->empty()) && (!segment[id]->deleted())) ) // not empty and not delete
+			while (!segment[id].empty() && !segment[id].deleted()) // not empty and not delete
 			{
 				id = ( id + get_hash_conflict(node) ) % N;
 			}
 
-			if ((segment[id]->empty()) || (segment[id]->deleted()))
+			if ((segment[id].empty()) || (segment[id].deleted()))
 			{
 				segment[id] = node;
+				log_info("HashTable", "insert", "true");
 				return true;
 			}
 			else
 			{
-				cout << "[insert] : failure : \n busy : ignore element";
+				log_error("HashTable", "insert", "false");
+				log_error("HashTable", "insert", "busy - ignore element");
 				return false;
 			}
 		}
 	}
 	else 
 	{ 
-		cout << "[insert] : key don't \n correct";
+		log_error("HashTable", "insert", "false");
+		log_error("HashTable", "insert", "key don't correct");
 		return false;
 	}
 }
 
-bool HashTable::remove(HashTableNode* node) 
+bool HashTable::remove(HashTableNode node) 
 {
 	if (correct_key(node))
 	{
-		int id = get_hash(node);
 		int pos = find_key(node);
 
 		if (pos == -1)
 		{
-			cout << "[remove] key don't \n ound";
+			log_error("HashTable", "remove", "key don't found");
 		}
-		else { segment[pos]->deleted(); return true; }
-	}
-	else { cout << "[remove] key don't correct \n "; }
-}
-
-int HashTable::find_key(HashTableNode* node)
-{
-	if (correct_key(node))
-	{
-		int id = get_hash(node);
-		int stop_id = id;
-
-		while ( segment[id] != node) 
+		else 
 		{
-			id = (id + get_hash_conflict(node)) % N;
-			if (id == stop_id) { break; }
+			log_info("HashTable", "remove", "key found");
+			return segment[pos].remove();
 		}
-
-		if (segment[id] == node) { return id; }
-		else { return -1; } // err 
 	}
-	else { cout << "[find key] key don't correct \n"; }
+	else 
+	{
+		log_error("HashTable", "remove", "key don't correct");
+		return false;
+	}
 }
 
-bool HashTable::correct_key(HashTableNode* node)
+int HashTable::find_key(HashTableNode node)
 {
-	// if (node.size() != 6) { return false; }
+	int id = get_hash(node);
 
-	// if (  is_char  (key[0])
-	// &&    is_num_c (key[1]) 
-	// &&    is_num_c (key[2])
-	// &&	  is_num_c (key[3])
-	// &&    is_char  (key[4])
-	// &&    is_char  (key[5]) ) { return true; }
+	while ( !(segment[id] == node) && !segment[id].empty() ) 
+	{
+		id = (id + get_hash_conflict(node)) % N;
+	}
 
-	// else { return false; }
+	if (segment[id] == node)
+	{
+		log_info("HashTable", "find_key", "true");
+		return id;
+	}
+	else
+	{
+		log_info("HashTable", "find_key", "false");
+		return -1;
+	}
+}
 
-
+bool HashTable::correct_key(HashTableNode node)
+{
+	// check to uniq
 
 	return true;
 }
 
-int HashTable::get_hash(HashTableNode* node)
+int HashTable::get_hash(HashTableNode node)
 {
-	char* key = node->get_key().get_reg();
+	char* key = node.data.get_reg_num().get_reg();
 
 	if (correct_key(node))
 	{
@@ -156,9 +159,9 @@ int HashTable::get_hash(HashTableNode* node)
 	}
 }
 
-int HashTable::get_hash_conflict(HashTableNode* node) 
+int HashTable::get_hash_conflict(HashTableNode node) 
 {
-	char* key = node->get_key().get_reg();
+	char* key = node.data.get_reg_num().get_reg();
 
 	if (correct_key(node))
 	{
@@ -177,14 +180,38 @@ int HashTable::get_hash_conflict(HashTableNode* node)
 	}
 }
 
-void HashTable::print(int from = 0, int to = 2500)
+void HashTable::print(int from, int to)
 {
 	if ( (0 <= from) && (to > N) ) { cout << "[print] : id don't correct \n"; return; }
 
-	int c = 0;
+	cout << endl << endl;
+	cout <<  "/ -------------------------------------------------------------------------------------------- \\ " << endl;
+	cout <<  "| ::::::::::::::::::::::::::::::::::::::::: Patients ::::::::::::::::::::::::::::::::::::::::: |  " << endl;
+	cout <<  "+ ------------- + --------------------- + ---------------------------------------- + --------- +  " << endl;
+	cout <<  "|      id       |  Registration number  |               Patient name               | Year born |  " << endl;
+
 	for (int i = from; i < to; i++)
 	{
-        // cout
+		if (!segment[i].empty()) {
+			cout <<  "+ ------------- + --------------------- + ---------------------------------------- + --------- +  " << endl;
+			cout.setf(ios::left);
+			cout << "|  "
+			<< setw(12) << i 											<< " |  " 
+			<< setw(20) << segment[i].data.get_reg_num().get_reg()		<< " |  " 
+			<< setw(39) << segment[i].data.get_name()					<< " |  " 
+			<< setw(8)  << segment[i].data.get_year_born()	 			<< " |  " 
+			<< endl;
+		}
 	}
+
+	cout << "\\ -------------------------------------------------------------------------------------------- /  " << endl;
+	cout << endl << endl;
+}
+
+
+
+bool operator==(HashTableNode p1, HashTableNode p2)
+{
+	return compare_str_equal(p1.data.get_reg_num().get_reg(), p2.data.get_reg_num().get_reg());
 }
 
